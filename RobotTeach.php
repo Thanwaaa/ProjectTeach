eac266c712b7f9d57459557ce0c7d1f72224bed1
Thanwaa <33487033+Thanwaa@users.noreply.github.com>
2017-11-08 22:35:38 +0700

    Update RobotTeach.php

--- RobotTeach.php
+++ RobotTeach.php
@@ -1,41 +1,40 @@
 <?php
-$strAccessToken = "K7xf/0yB0VW0ReOg0vNSjdi2iguCM78VAs3W3wLHSeAXv9Z1gREADn/2U+IjjpVFaLU2hP5nSiwyxskB5f+y1dtkjlY6Jm+85rZyM18BgXTwHknJQNjoobsNlL7nQpiB/iYgEWUfiPmyYpWU1d2JBwdB04t89/1O/w1cDnyilFU=";
+ 
+$strAccessToken = "K7xf/0yB0VW0ReOg0vNSjdi2iguCM78VAs3W3wLHSeAXv9Z1gREADn/2U+IjjpVFaLU2hP5nSiwyxskB5f+y1dtkjlY6Jm+85rZyM18BgXTwHknJQNjoobsNlL7nQpiB/iYgEWUfiPmyYpWU1d2JBwdB04t89/1O/w1cDnyilFU=
+";
+ 
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
+ 
 $strUrl = "https://api.line.me/v2/bot/message/reply";
-$_userId = $arrJson['events'][0]['source']['userId'];
-$_msg = $arrJson['events'][0]['message']['text'];
+ 
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
-$filename = 'text.txt';
-if (file_exists($filename)) {
-$myfile = fopen('abc.txt', "w+") or die("Unable to open file!");
-fwrite($myfile, $_msg);
-fclose($myfile);
-} else {
-$myfile = fopen('abc.txt', "x+") or die("Unable to open file!");
-fwrite($myfile, $_msg);
-fclose($myfile);
-}
+ 
 if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
-$arrPostData = array();
-$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-$arrPostData['messages'][0]['type'] = "text";
-$arrPostData['messages'][0]['text'] = "สวัสดี ID ของคุณคือ ".$arrJson['events'][0]['source']['userId'];
-}else if($arrJson['events'][0]['message']['text'] == "123456789") // เป็นรหัสเปลดล็อด
-{
-$arrPostData = array();
-$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-$arrPostData['messages'][0]['type'] = "text";
-$arrPostData['messages'][0]['text'] = "เรียบร้อยคับ";
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
+}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
+}else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
 }else{
-$arrPostData = array();
-$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-$arrPostData['messages'][0]['type'] = "text";
-$arrPostData['messages'][0]['text'] = "รหัสไม่ถูกต้อง";
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 }
-
+ 
+ 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
@@ -46,4 +45,5 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
+ 
 ?>
