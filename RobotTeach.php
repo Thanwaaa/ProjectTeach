ac1bd451ed10b1d0fade0556fe147f55fb88747f
Thanwaa <33487033+Thanwaa@users.noreply.github.com>
2017-11-08 22:51:00 +0700

    Update RobotTeach.php

--- RobotTeach.php
+++ RobotTeach.php
@@ -1,51 +1,69 @@
-
- 
-
- 
 <?php
 $access_token = 'K7xf/0yB0VW0ReOg0vNSjdi2iguCM78VAs3W3wLHSeAXv9Z1gREADn/2U+IjjpVFaLU2hP5nSiwyxskB5f+y1dtkjlY6Jm+85rZyM18BgXTwHknJQNjoobsNlL7nQpiB/iYgEWUfiPmyYpWU1d2JBwdB04t89/1O/w1cDnyilFU=';
 
-// Get POST body content
 $content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
+$arrJson = json_decode($content, true);
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+$_userId = $arrJson['events'][0]['source']['userId'];
+$_msg = $arrJson['events'][0]['message']['text'];
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+$filename = 'text.txt';
+if (file_exists($filename)) {
+$myfile = fopen('text.txt', "w+") or die("Unable to open file!");
+fwrite($myfile, $_msg);
+fclose($myfile);
+} else {
+$myfile = fopen('text.txt', "x+") or die("Unable to open file!");
+fwrite($myfile, $_msg);
+fclose($myfile);
 }
-echo "OK"
+if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
+$arrPostData = array();
+$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "สวัสดี ID ของคุณคือ ".$arrJson['events'][0]['source']['userId'];
+}else if($arrJson['events'][0]['message']['text'] == "turnleft"){
+$arrPostData = array();
+$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "Bot : เลี้ยวซ้าย";
+}else if($arrJson['events'][0]['message']['text'] == "turnright"){
+$arrPostData = array();
+$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "Bot : เลี้ยวขวา";
+}else if($arrJson['events'][0]['message']['text'] == "forback"){
+$arrPostData = array();
+$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "Bot : ถอยหลัง";
+}else if($arrJson['events'][0]['message']['text'] == "forward"){
+$arrPostData = array();
+$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "Bot : ตรงไปข้างหน้า";
+}else if($arrJson['events'][0]['message']['text'] == "stop"){
+$arrPostData = array();
+$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "Bot : หยุด";
+}else{
+$arrPostData = array();
+$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "Bot :ไม่เข้าใจคำสั่ง";
+}
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+?>
